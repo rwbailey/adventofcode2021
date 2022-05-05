2f1c0b9872ace1e4855ea16dad68776cabb2c1d3
Richard Bailey <richard@richardbailey.xyz>
2022-05-05 23:59:36 +0100

    add readme

---
+++ day3/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/rwbailey/advent2021/helpers"
+)
+
+func main() {
+	part1()
+	// part2()
+}
+
+func part1() {
+	data := helpers.GetInput("3")
+	fmt.Println(data)
+}
+
+func part2() {
+	// data := helpers.GetInput("3")
+	// fmt.Println()
+}
+
+func powerConsumption(data string) int {
+	ones, zeros := 0, 0
+	strings := helpers.MustStringList(data, "\n")
+	return 0
+}
