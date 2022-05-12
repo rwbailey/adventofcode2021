afbf9cb234607dc7be5239f61f276f1377374854
Richard Bailey <richard@richardbailey.xyz>
2022-05-12 23:27:57 +0100

    Day 3 Part 2

--- day3/main.go
+++ day3/main.go
@@ -9,7 +9,7 @@ import (
 
 func main() {
 	part1()
-	// part2()
+	part2()
 }
 
 func part1() {
@@ -18,8 +18,62 @@ func part1() {
 }
 
 func part2() {
-	// data := helpers.GetInput("3")
-	// fmt.Println()
+	data := helpers.GetInput("3")
+	fmt.Println(lifeSupport(data))
+}
+
+func lifeSupport(data string) int64 {
+	strings := helpers.MustStringList(data, "\n")
+
+	o2, co2 := lsr(0, strings, strings)
+
+	a, _ := strconv.ParseInt(o2[0], 2, 64)
+	b, _ := strconv.ParseInt(co2[0], 2, 64)
+
+	return a * b
+}
+
+func lsr(n int, o2, co2 []string) ([]string, []string) {
+	if len(o2) == 1 && len(co2) == 1 {
+		return o2, co2
+	}
+	// oxy, carb := o2, co2
+
+	ones, zeroes := []string{}, []string{}
+
+	if len(o2) > 1 {
+		for _, v := range o2 {
+			if string(v[n]) == "1" {
+				ones = append(ones, v)
+			} else if string(v[n]) == "0" {
+				zeroes = append(zeroes, v)
+			}
+		}
+		if len(zeroes) > len(ones) {
+			o2 = zeroes
+		} else {
+			o2 = ones
+		}
+	}
+
+	ones, zeroes = []string{}, []string{}
+
+	if len(co2) > 1 {
+		for _, v := range co2 {
+			if string(v[n]) == "1" {
+				ones = append(ones, v)
+			} else if string(v[n]) == "0" {
+				zeroes = append(zeroes, v)
+			}
+		}
+		if len(ones) < len(zeroes) {
+			co2 = ones
+		} else {
+			co2 = zeroes
+		}
+	}
+
+	return lsr(n+1, o2, co2)
 }
 
 func powerConsumption(data string) int64 {
